795441462e9bb0971c4b07261fd0a9281a135765
Hallee Shearer <89100433+halleeshearer@users.noreply.github.com>
2024-08-09 20:09:00 -0400

    Update script2viz.py

--- script2viz.py
+++ script2viz.py
@@ -6,7 +6,7 @@ import base64
 
 #########################
 # EDIT THESE PARAMETERS #
-script_path = 'neurohackademy.py'
+script_path = 'scripts/neurohackademy.py'
 
 # Optional:
 top_down = True # True for plotting the flowchart vertically, False for horizontally left to right
